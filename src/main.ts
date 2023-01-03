9e7d6f0678a2554a96f0b0f50cf5ad897d139afb
Antony Pereira <antony0000@live.com>
2023-01-03 09:33:17 -0300

    feat: - created prisma module and service - added class validator and class tranformer global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      transform: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
